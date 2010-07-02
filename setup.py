bc9128329b22741e01868dccdb5ad6719869a167
rogeriopvl <rogeriopvl@gmail.com>
2010-07-02 20:53:03 +0100

    Started adding pip package file structure.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup (
+	name = "downpy",
+	packages = ["downpy"],
+	version = "1.0",
+	description =  "Downloads all files from a web page.",
+	author = "Rogerio Vicente",
+	author_email = "rogeriopvl@gmail.com"
+	url = "http://github.com/rogeriopvl/downpy",
+	download_url = "",
+	keywords = "",
+	classifiers = [],
+	long_description = ""
+)
