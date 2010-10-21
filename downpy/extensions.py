fcc5f5eef929b33dffab1a3be2fa34ed933d38e2
rogeriopvl <rogeriopvl@gmail.com>
2010-10-21 21:54:13 +0100

    Added extensions module containing lists of possible extensions.

---
+++ downpy/extensions.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# some filetypes are incomplete but this should do it for now
+audio = ['.mp3', '.oga', '.ogg', '.mp4a', '.wma', '.aac', '.mid']
+video = ['.avi', '.flv', '.mp4', '.mkv', '.ogv', '.webm', '.wmv']
+doc = ['.chm', '.doc', '.docx', '.epub', '.mobi', '.odf', '.pdf', '.rtf', '.txt']
