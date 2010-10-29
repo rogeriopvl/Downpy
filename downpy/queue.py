21c4d1b7ea68b953e7bdc663ffb3db31d5b8fae4
rogeriopvl <rogeriopvl@gmail.com>
2010-10-30 00:09:26 +0100

    Added try-catch to recover method

--- downpy/queue.py
+++ downpy/queue.py
@@ -16,13 +16,22 @@ class Queue(object):
 		self.items = deque()
 	
 	def add(self, url):
+		"""Adds an element to the queue"""
 		self.items.append(url)
 	
 	def next(self):
+		"""Get the next element in the queue"""
 		try:
 			return self.items.popleft()
 		except IndexError:
 			return False
 	
 	def recover(self, serializedFile):
-		self.items = deque(open(serializedFile))
+		"""Opens a serialized queue object in a file and rebuilds it"""
+		try:
+			fd = open(serializedFile)
+		except:
+			return False
+
+		self.items = deque(fd)
+		return True
