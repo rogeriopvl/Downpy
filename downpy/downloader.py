81bd82e766b712f43e8b4ca2d87d1d7f8de10ba6
David Cruz <devel.david@vcruz.net>
2010-11-01 00:17:25 +0000

    Change download to use urllib.urlretrieve
    urllib.urlretrieve allows to obtain progress information.

--- downpy/downloader.py
+++ downpy/downloader.py
@@ -4,17 +4,13 @@ File: downloader.py
 Author: Rogerio Vicente <http://rogeriopvl.com>
 Description: Module for functions related to file downloading
 '''
-import urllib2
+import urllib
 import parser
 
 def downloadFile(url, folder):
-	"""Dowloads files from urls"""
-	try:
-		res = urllib2.urlopen(url)
-		f = open(folder+"/"+parser.parseFilename(url), "wb")
-		for line in res:
-			f.write(line)
-		f.close()
-	except:
-		print "Error: invalid url"
-		exit()
+    """Dowloads files from urls"""
+    try:
+        fname, headers = urllib.urlretrieve(url, folder+"/"+parser.parseFilename(url))
+    except:
+        print "Error: invalid url"
+        exit()
